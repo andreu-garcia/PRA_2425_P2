f4644d7411abaffd9e37013d517a9ffb1bc21a28
Andreu Garcia Sendra <andreu.garcia@gmail.com>
2025-11-10 15:16:02 +0100

    alguns canvis mes

--- main.cpp
+++ main.cpp
@@ -1,40 +1,59 @@
 #include "DyV.h"
+#include <iostream>
+#include <vector>
 using namespace std;
 
+int main() {
+    cout << "Tamany del vector: ";
+    int n;
+    cin >> n;
 
-int main(){
+    vector<int> v1(n);
+    cout << "Introdueix els números per al vector v1:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> v1[i];
+    }
 
-	cout << "Tamany del vector: ";
-	int n; cin >> n;
-	vector <int> v1(n);
+    int a;
+    cout << "Introdueix el número que vols buscar: ";
+    cin >> a;
+    cout << "\n";
 
-	cout << "Dame números/palabras para guardarlas en el vector v1:";
-	for(int i = 0; i < n; i++){cin >> v1[i];}
-	int a; cout << "Dame el número a buscar: "; cin >> a;
-	cout << "\n";
-	int res = BusquedaBinaria(a, v1, 0, n - 1);
-	cout << "El numero esta en la posicion: " << res << endl;
-	
-    cout << "Dame el tamaño del vector: ";
-    int z; cin >> z;
-	
-	vector <float> v2(z);
-    cout << "Dame números/palabras para guardarlas en el vector v2:";
-    
-	for(int i = 0; i < z; i++) {cin >> v2[i];}
-	float resF = BusquedaBinaria(2.0f, v2, 0, z - 1);
-	cout << "El numero esta en la posicion: " << resF << endl;
-	cout << "\n";
+    int res = BusquedaBinaria(a, v1, 0, n - 1);
+    if (res != -1)
+        cout << "El número es troba en la posició: " << res << endl;
 
-	cout << "Dame el tamaño del vector: ";
-	int m; cin >> m;
-	
-	vector <int> v3(m);
-	cout << "Dame números para guardarlas en el vector v3:";
-	
-	for(int i = 0; i < m; i++) {cin >> v3[i];}
-	QuickSort(v3, 0, m - 1);
+    cout << "\nTamany del segon vector: ";
+    int z;
+    cin >> z;
 
-	cout << "\n";
-	return 0;
+    vector<float> v2(z);
+    cout << "Introdueix els números per al vector v2:\n";
+    for (int i = 0; i < z; i++) {
+        cin >> v2[i];
+    }
+
+    float resF = BusquedaBinaria(2.0f, v2, 0, z - 1);
+    if (resF != -1)
+        cout << "El número 2.0 es troba en la posició: " << resF << endl;
+
+    cout << "\nTamany del tercer vector: ";
+    int m;
+    cin >> m;
+
+    vector<int> v3(m);
+    cout << "Introdueix els números per al vector v3:\n";
+    for (int i = 0; i < m; i++) {
+        cin >> v3[i];
+    }
+
+    QuickSort(v3, 0, m - 1);
+
+    cout << "Vector ordenat:\n";
+    for (int i = 0; i < m; i++) {
+        cout << v3[i] << " ";
+    }
+    cout << "\n";
+
+    return 0;
 }
